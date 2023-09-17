fde2174d02d0c99c8cab6fd68129e3c848c9e28e
V-italiy <108426033+V-italiy@users.noreply.github.com>
2023-09-17 13:01:57 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// for (
+//   var i = 0;
+//   i < document.querySelectorAll(".image-collection").length;
+//   i++
+// ) {
+//   document
+//     .querySelectorAll(".image-collection")
+//     [i].addEventListener("click", function () {
+//       alert("I got clicked!");
+//     });
+// }
